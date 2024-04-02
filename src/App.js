ebf65d7b5311fdfb13b74d4628fc51b7275117a1
ilyas abduttawab <ilyas@stickearn.com>
2024-04-03 04:59:46 +0700

    first init, fetch users and delete user

---
+++ src/App.js
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+import { useTable } from 'react-table'
+
+import makeData from './makeData'
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+
+  const fetchUsers = () => {
+    fetch('http://127.0.0.1:8000/api/users')
+    .then((response) => response.json())
+    .then((data) => {
+
+      const parsedData = data.data.map((user) => ({
+        uuid: user.uuid,
+        firstName: JSON.parse(user.name).first,
+        lastName: JSON.parse(user.name).last,
+        gender: user.gender,
+        age: user.age,
+        location: user.location,
+      }));
+      console.log(parsedData)
+       setPosts(parsedData);
+    })
+    .catch((err) => {
+       console.log(err.message);
+    });
+
+  }
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Gender',
+            accessor: 'gender',
+          },
+          {
+            Header: 'Location',
+            accessor: 'location',
+          },
+          {
+            Header: 'Action',
+            accessor: 'delete',
+            Cell: ({ row }) => (
+              <button onClick={() => handleDelete(row.original.uuid)}>Delete</button>
+            ),
+  
+          },
+
+        ],
+      },
+    ],
+    []
+  )
+
+  const handleDelete = (uuid) => {
+    // Handle delete action
+    console.log(uuid)
+      fetch(`http://127.0.0.1:8000/api/users/${uuid}`, {
+        method: 'DELETE',
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        // If the response is successful, perform any necessary actions
+        console.log('User deleted successfully');
+        fetchUsers()
+
+        // You may want to update the UI or state here
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+        // Handle any errors that occurred during the fetch
+      });
+    };
+  
+  return (
+    <Styles>
+      <Table columns={columns} data={posts} />
+    </Styles>
+  )
+}
+
+export default App
