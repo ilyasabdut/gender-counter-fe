fdc480baf1029f19122ed8ddef538c3d5e8fe032
ilyasabdut <ilyasabduttawab@gmail.com>
2024-04-03 13:30:45 +0000

    Deploying to main from @ ilyasabdut/peasy-gender-counter-fe@6cb58ae791809bd31a4c7a83ad4f5e278b6f46cd 🚀

---
+++ precache-manifest.016e4325ee50829e8844ffb5e59f2895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfb1f62661223a30f5a3bb3c763bbbf",
+    "url": "/peasy-gender-counter-fe/index.html"
+  },
+  {
+    "revision": "bfa6fa727c56d5deb5d4",
+    "url": "/peasy-gender-counter-fe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a3b9eeb37c909a4e63e",
+    "url": "/peasy-gender-counter-fe/static/js/2.1b89a40f.chunk.js"
+  },
+  {
+    "revision": "bfa6fa727c56d5deb5d4",
+    "url": "/peasy-gender-counter-fe/static/js/main.766cd51c.chunk.js"
+  },
+  {
+    "revision": "889a8ffd8c54d8a96842",
+    "url": "/peasy-gender-counter-fe/static/js/runtime~main.5c30c5b3.js"
+  }
+]);
